bdfc08d6a81435a1bc9320568c41d71d20b7a919
Alex Williamson <alewilliam789@gmail.com>
2024-08-15 10:53:31 -0700

    Add the country names to ingest the regions/provinces

--- transform_arrival_data.py
+++ transform_arrival_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sqlalchemy import text
 from engine import get_engine
 from s3 import download_file
 from engine import get_engine
@@ -12,18 +13,20 @@ eng = get_engine("TravelSTAGE")
 
 arrival_data = pd.read_csv(csv_file,skiprows=4)
 
-select_columns = ["Country Code"]
+select_columns = ["Country Name","Country Code"]
 
 for i in range(1995,2021):
     select_columns.append(f"{i}")
 
 arrival_data = arrival_data[select_columns]
-new_arrival_data = pd.DataFrame(columns=["CountryCode", "ArrivalYear","TouristAmount"])
+new_arrival_data = pd.DataFrame(columns=["CountryCode", "ArrivalYear","TouristAmount", "CountryName"])
 
 for row in arrival_data.iterrows():
     index = row[0]
+
+    country_name = row[1]["Country Name"]
     country_code = row[1]["Country Code"]
-    series = row[1].drop(["Country Code"], axis=0)
+    series = row[1].drop(["Country Name","Country Code"], axis=0)
     
     
     new_years = series.to_frame().reset_index()
@@ -31,16 +34,20 @@ for row in arrival_data.iterrows():
     new_columns["index"] = "ArrivalYear"
     new_columns[index] = "TouristAmount"
     new_years = new_years.rename(columns=new_columns)
+    new_years["CountryName"] = country_name
     new_years["CountryCode"] = country_code
     new_years["TouristAmount"] = new_years["TouristAmount"].astype(float)
     new_years["ArrivalYear"] = new_years["ArrivalYear"].astype(int)
-    new_arrival_data = pd.concat([new_arrival_data,new_years[["CountryCode","ArrivalYear","TouristAmount"]]])
+    new_arrival_data = pd.concat([new_arrival_data,new_years[["CountryCode","ArrivalYear","TouristAmount", "CountryName"]]])
+
+
 
 
 
 with eng.begin() as conn:
-    new_arrival_data.to_sql(name="STAGE_NoFillArrival", con=conn, if_exists='fail')
+    conn.execute(text("TRUNCATE TABLE TravelSTAGE.dbo.STAGE_NoFillArrival"))
     
+    new_arrival_data.to_sql("STAGE_NoFillArrival", conn, if_exists='append')
 
 
 os.remove(csv_file)
